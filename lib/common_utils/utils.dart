34f1c67b51c9f16fc4205b6f2d11bc2c15a5621f
Shivam JhunJhunwala <shivamjhunjhunwala4dec@gmail.com>
2022-06-25 02:23:24 +0530

    profile_page

---
+++ lib/common_utils/utils.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyRoute {
+  static String profileRoute = "/profile";
+}
+
+hexStringToColor(String hexColor) {
+  hexColor = hexColor.toUpperCase().replaceAll("#", "");
+  if (hexColor.length == 6) {
+    hexColor = "FF" + hexColor;
+  }
+  return Color(int.parse(hexColor, radix: 16));
+}
