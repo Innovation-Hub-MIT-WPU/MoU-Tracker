8cb11053927ddbdf229dbe58dde278dda86e91af
Prathamesh Chavan <prathamesh.chavanpsc2018@gmail.com>
2022-07-19 20:21:38 +0530

    stats page pdf printing

---
+++ lib/screens/home_page/main_tabs/stats_page/preview_screen.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
+import 'package:MouTracker/common_utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+class PreviewScreen extends StatelessWidget {
+  final pw.Document doc;
+
+  const PreviewScreen({Key? key, required this.doc}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(context),
+      body: PdfPreview(
+        build: (format) => doc.save(),
+        allowSharing: true,
+        allowPrinting: true,
+        initialPageFormat: PdfPageFormat.a4,
+        pdfFileName: "mydoc.pdf",
+      ),
+    );
+  }
+
+  AppBar appBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+          size: 30,
+        ),
+        onPressed: () => Navigator.pop(context),
+      ),
+      backgroundColor: hexStringToColor("2D376E"),
+      bottom: PreferredSize(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+            child: Text(
+              "Preview",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: Theme.of(context).textTheme.headline2!.fontFamily,
+                fontSize: Theme.of(context).textTheme.headline3!.fontSize,
+              ),
+            ),
+          ),
+          preferredSize:
+              Size.fromHeight(MediaQuery.of(context).size.height * 0.1)),
+      centerTitle: true,
+    );
+  }
+}
--- lib/screens/home_page/main_tabs/stats_page/stats_page.dart
+++ lib/screens/home_page/main_tabs/stats_page/stats_page.dart
@@ -1,9 +1,13 @@
-// ignore_for_file: sort_child_properties_last, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: sort_child_properties_last, prefer_const_constructors, prefer_const_literals_to_create_immutables, use_build_context_synchronously
 
 import 'package:MouTracker/common_utils/utils.dart';
+import 'package:MouTracker/screens/home_page/main_tabs/stats_page/preview_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:intl/intl.dart';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class StatsPage extends StatefulWidget {
   const StatsPage({Key? key}) : super(key: key);
@@ -12,27 +16,83 @@ class StatsPage extends StatefulWidget {
 }
 
 class StatsPageState extends State<StatsPage> {
-  late List<SalesData> _chartData;
+  static late GlobalKey<SfCartesianChartState> cartesianChartKey1;
+  // static late GlobalKey<SfCartesianChartState> cartesianChartKey2;
+  static late GlobalKey<SfCartesianChartState> cartesianChartKey3;
+  late List<SalesData> _chartData1;
+  late List<ChartData> _chartData2;
   late TooltipBehavior _tooltipBehavior;
+
   @override
   void initState() {
-    _chartData = getChartData();
+    // Timer.periodic(Duration(days: 30), updateDataSource);
+    cartesianChartKey1 = GlobalKey();
+    // cartesianChartKey2 = GlobalKey();
+    cartesianChartKey3 = GlobalKey();
+    _chartData1 = getChartData1();
+    _chartData2 = getChartData2();
     _tooltipBehavior = TooltipBehavior(enable: true);
     super.initState();
   }
 
-  final List<ChartData> chartData = [
-    ChartData('David', 12.5, Colors.black),
-    ChartData('Steve', 100 - 12.5, hexStringToColor("C4C4C4")),
+  //int index = 0;
+  // void updateDataSource(Timer timer) {
+  //   _chartData1.add(SalesData(monthsName[index++], approvedMous[index++]));
+  //   _chartData1.remove(0);
+  // }
+
+  List<double> approvedMous = [
+    21,
+    24,
+    35,
+    38,
+    54,
+    21,
+    24,
+    35,
+    38,
+    54,
+    38,
+    54,
   ];
 
-  List<SalesData> getChartData() {
+  List<String> monthsName = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+
+  List<ChartData> getChartData2() {
+    final List<ChartData> chartData2 = [
+      ChartData('Approved', 12.5, Colors.black),
+      ChartData('Not  Approved', 100 - 12.5, hexStringToColor("C4C4C4")),
+    ];
+    return chartData2;
+  }
+
+  List<SalesData> getChartData1() {
     final List<SalesData> chartData = [
-      SalesData(2017, 25),
-      SalesData(2018, 12),
-      SalesData(2019, 24),
-      SalesData(2020, 18),
-      SalesData(2021, 30)
+      SalesData('Jan', 21),
+      SalesData('Feb', 24),
+      SalesData('Mar', 35),
+      SalesData('Apr', 38),
+      SalesData('May', 54),
+      SalesData('Jun', 21),
+      SalesData('Jul', 24),
+      SalesData('Aug', 35),
+      SalesData('Sep', 38),
+      SalesData('Oct', 54),
+      SalesData('Nov', 38),
+      SalesData('Dec', 54)
     ];
     return chartData;
   }
@@ -40,6 +100,8 @@ class StatsPageState extends State<StatsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: cartesianChartKey3,
+      floatingActionButton: SavePDF(),
       appBar: appBar(),
       body: SingleChildScrollView(
         child: Column(
@@ -56,15 +118,15 @@ class StatsPageState extends State<StatsPage> {
                 child: lineChart(),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.03),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.35,
-                width: MediaQuery.of(context).size.width,
-                child: approvalRate(),
-              ),
-            )
+            // Padding(
+            //   padding: EdgeInsets.only(
+            //       top: MediaQuery.of(context).size.height * 0.03),
+            //   child: SizedBox(
+            //     height: MediaQuery.of(context).size.height * 0.35,
+            //     width: MediaQuery.of(context).size.width,
+                // child: approvalRate(),
+            //   ),
+            // )
           ],
         ),
       ),
@@ -82,58 +144,84 @@ class StatsPageState extends State<StatsPage> {
               style: TextStyle(
                 color: Colors.white,
                 fontFamily: Theme.of(context).textTheme.headline2!.fontFamily,
-                fontSize: Theme.of(context).textTheme.headline4!.fontSize,
+                fontSize: Theme.of(context).textTheme.headline3!.fontSize,
               ),
             ),
           ),
           preferredSize:
-              Size.fromHeight(MediaQuery.of(context).size.height * 0.02)),
+              Size.fromHeight(MediaQuery.of(context).size.height * 0.1)),
       centerTitle: true,
     );
   }
 
-  Widget approvalRate() {
-    return SfCircularChart(
-      backgroundColor: hexStringToColor("EDF9FF"),
-      title: ChartTitle(text: "Aprroval Rate"),
-      series: <CircularSeries>[
-        DoughnutSeries<ChartData, String>(
-            dataSource: chartData,
-            pointColorMapper: (ChartData data, _) => data.color,
-            xValueMapper: (ChartData data, _) => data.x,
-            yValueMapper: (ChartData data, _) => data.y)
-      ],
-    );
-  }
+  // Widget approvalRate() {
+  //   return SfCircularChart(
+  //     key: cartesianChartKey2,
+  //     backgroundColor: hexStringToColor("EDF9FF"),
+  //     title: ChartTitle(
+  //         text: "Approval Rate",
+  //         textStyle: TextStyle(fontWeight: FontWeight.bold)),
+  //     annotations: <CircularChartAnnotation>[
+  //       CircularChartAnnotation(
+  //         widget: Container(
+  //           child: const Text(
+  //             '12.5%',
+  //             style: TextStyle(
+  //                 color: Color.fromRGBO(0, 0, 0, 0.5),
+  //                 fontSize: 25,
+  //                 fontWeight: FontWeight.bold),
+  //           ),
+  //         ),
+  //       ),
+  //     ],
+  //     series: <CircularSeries>[
+  //       DoughnutSeries<ChartData, String>(
+  //           strokeWidth: 1,
+  //           dataSource: _chartData2,
+  //           pointColorMapper: (ChartData data, _) => data.color,
+  //           xValueMapper: (ChartData data, _) => data.x,
+  //           yValueMapper: (ChartData data, _) => data.y)
+  //     ],
+  //   );
+  // }
 
-  Widget lineChart() {
+  SfCartesianChart lineChart() {
     return SfCartesianChart(
+      //isTransposed: true,
+      key: cartesianChartKey1,
       backgroundColor: hexStringToColor("EDF9FF"),
-      title: ChartTitle(text: "Monthly Approved MOU's"),
+      title: ChartTitle(
+          text: "Monthly Approved MOU's",
+          textStyle: TextStyle(fontWeight: FontWeight.bold)),
       //legend: Legend(isVisible: true),
       tooltipBehavior: _tooltipBehavior,
       series: <ChartSeries>[
-        SplineSeries<SalesData, double>(
+        SplineSeries<SalesData, String>(
           //name: 'Sales',
+          markerSettings: MarkerSettings(isVisible: true),
           color: Colors.black,
-          dataSource: _chartData,
-          xValueMapper: (SalesData sales, _) => sales.year,
+          dataSource: _chartData1,
+          xValueMapper: (SalesData sales, _) => sales.month,
           yValueMapper: (SalesData sales, _) => sales.sales,
           dataLabelSettings: DataLabelSettings(isVisible: true),
           enableTooltip: true,
         )
       ],
-      primaryXAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
-      primaryYAxis: NumericAxis(
-          labelFormat: '{value}M',
-          numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)),
+      primaryXAxis: CategoryAxis(
+        name: "Months",
+        edgeLabelPlacement: EdgeLabelPlacement.shift,
+        maximumLabels: 5,
+      ),
+      // primaryYAxis: NumericAxis(
+      //     labelFormat: '{value}M',
+      //     numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)),
     );
   }
 }
 
 class SalesData {
-  SalesData(this.year, this.sales);
-  final double year;
+  SalesData(this.month, this.sales);
+  final String month;
   final double sales;
 }
 
@@ -142,4 +230,92 @@ class ChartData {
   final String x;
   final double y;
   final Color color;
+}
+
+class SavePDF extends StatefulWidget {
+  const SavePDF({Key? key}) : super(key: key);
+
+  @override
+  State<SavePDF> createState() => SavePDFState();
+}
+
+class SavePDFState extends State<SavePDF> {
+  @override
+  Widget build(BuildContext context) {
+    return _floating();
+  }
+
+  FloatingActionButton _floating() {
+    return FloatingActionButton(
+      onPressed: displayPdf,
+      backgroundColor: Colors.black,
+      child: const Icon(Icons.picture_as_pdf),
+    );
+  }
+
+  void displayPdf() async {
+    final ui.Image data1 = await StatsPageState.cartesianChartKey1.currentState!
+        .toImage(pixelRatio: 3.0);
+    final ByteData? bytes1 =
+        await data1.toByteData(format: ui.ImageByteFormat.png);
+    final Uint8List imageBytes1 =
+        bytes1!.buffer.asUint8List(bytes1.offsetInBytes, bytes1.lengthInBytes);
+
+    // final ui.Image data2 = await (StatsPageState.cartesianChartKey1.currentState as dynamic)
+    //     .toImage(pixelRatio: 3.0);
+    // final ByteData? bytes2 =
+    //     await data2.toByteData(format: ui.ImageByteFormat.png);
+    // final Uint8List imageBytes2 =
+    //     bytes2!.buffer.asUint8List(bytes2.offsetInBytes, bytes2.lengthInBytes);
+
+    final doc = pw.Document();
+
+    doc.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return buildPrintableData(imageBytes1);
+        },
+      ),
+    );
+
+    /// open Preview Screen
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PreviewScreen(doc: doc),
+        ));
+  }
+
+  buildPrintableData(Uint8List imageBytes1) {
+    final image1 = pw.MemoryImage(imageBytes1);
+    // final image2 = pw.MemoryImage(imageBytes2);
+    return pw.Wrap(
+      alignment: pw.WrapAlignment.center,
+      crossAxisAlignment: pw.WrapCrossAlignment.start,
+      runSpacing: 20,
+      children: [
+        pw.Container(
+          width: MediaQuery.of(context).size.width * 1.25,
+          height: 500,
+          decoration: pw.BoxDecoration(
+            image: pw.DecorationImage(
+              fit: pw.BoxFit.fill,
+              image: image1,
+            ),
+          ),
+        ),
+        // pw.Container(
+        //   width: MediaQuery.of(context).size.width * 1.25,
+        //   height: 350,
+        //   decoration: pw.BoxDecoration(
+        //     image: pw.DecorationImage(
+        //       fit: pw.BoxFit.fill,
+        //       image: image2,
+        //     ),
+        //   ),
+        // ),
+      ],
+    );
+  }
 }
