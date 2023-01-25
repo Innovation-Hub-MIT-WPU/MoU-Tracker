eb00e10b65d15690ebef44667289c53635129850
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2023-01-25 09:31:11 +0530

    webview issue

--- lib/screens/mou_details/track_page_utils/web_view.dart
+++ lib/screens/mou_details/track_page_utils/web_view.dart
@@ -1,42 +1,42 @@
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: WebViewApp(),
-    ),
-  );
-}
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       home: WebViewApp(),
+//     ),
+//   );
+// }
 
-class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
+// class WebViewApp extends StatefulWidget {
+//   const WebViewApp({super.key});
 
-  @override
-  State<WebViewApp> createState() => _WebViewAppState();
-}
+//   @override
+//   State<WebViewApp> createState() => _WebViewAppState();
+// }
 
-class _WebViewAppState extends State<WebViewApp> {
-  late final WebViewController controller;
+// class _WebViewAppState extends State<WebViewApp> {
+//   late final WebViewController controller;
 
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController()
-      ..loadRequest(
-        Uri.parse('https://www.flutter.dev'),
-      );
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     controller = WebViewController()
+//       ..loadRequest(
+//         Uri.parse('https://www.flutter.dev'),
+//       );
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter WebView'),
-      ),
-      body: WebViewWidget(
-        controller: controller,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Flutter WebView'),
+//       ),
+//       body: WebViewWidget(
+//         controller: controller,
+//       ),
+//     );
+//   }
+// }
