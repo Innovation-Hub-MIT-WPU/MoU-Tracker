5ffe7b5f569d7e32031bb943512d04537a272453
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2023-02-03 07:12:50 +0530

    feat: engagement activities setup

---
+++ lib/screens/engagement_pages/placement_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlacementData extends StatefulWidget {
+  const PlacementData({super.key});
+
+  @override
+  State<PlacementData> createState() => _PlacementDataState();
+}
+
+class _PlacementDataState extends State<PlacementData> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
